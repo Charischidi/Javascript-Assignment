d4724c7afccfaa6b3bd205e5e521b544c848ac88
CHIDI CHIEDOZIE OKONKWO <claymoor85@gmail.com>
2022-07-15 12:02:04 +0000

    I re-write countEvenNumbers function.

--- src/assignment.js
+++ src/assignment.js
@@ -33,7 +33,13 @@ assignment.sumOfNumbers = sumOfNumbers;
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return (arrayOfNumbers % 2 == 0);
+  let count = 0;
+	for(let i = 0 ; i < assignment.length; i++) {
+		if (assignment[i] % 2 === 0){
+			count++;
+		}
+	}
+	return count;
 }
 assignment.countEvenNumbers = countEvenNumbers;
 
